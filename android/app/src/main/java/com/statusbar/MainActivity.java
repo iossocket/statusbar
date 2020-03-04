9273e4c26526d0c4ad228d73e8adf171e244617d
iossocket <avx302@gmail.com>
2020-03-04 23:08:44 +0800

    init

---
+++ android/app/src/main/java/com/statusbar/MainActivity.java
@@ -0,0 +1,39 @@
+package com.statusbar;
+
+import android.graphics.Color;
+import android.os.Build;
+import android.os.Bundle;
+import android.view.View;
+
+import com.facebook.react.ReactActivity;
+import com.facebook.react.ReactActivityDelegate;
+import com.facebook.react.ReactRootView;
+import com.swmansion.gesturehandler.react.RNGestureHandlerEnabledRootView;
+
+public class MainActivity extends ReactActivity {
+
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        View decorView = getWindow().getDecorView();
+        decorView.setSystemUiVisibility(View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN | View.SYSTEM_UI_FLAG_LAYOUT_STABLE);
+        if (Build.VERSION.SDK_INT >= 21) {
+            getWindow().setStatusBarColor(Color.TRANSPARENT);
+        }
+    }
+
+    @Override
+    protected String getMainComponentName() {
+        return "statusbar";
+    }
+
+    @Override
+    protected ReactActivityDelegate createReactActivityDelegate() {
+        return new ReactActivityDelegate(this, getMainComponentName()) {
+            @Override
+            protected ReactRootView createRootView() {
+                return new RNGestureHandlerEnabledRootView(MainActivity.this);
+            }
+        };
+    }
+}
---
+++ src/pages/Home.js
@@ -0,0 +1,48 @@
+import React from 'react'
+import { StyleSheet, View, ImageBackground, Button, StatusBar } from 'react-native'
+
+import Header from '../components/Header'
+
+export default Home = (props) => {
+  return (
+    <View style={styles.fill}>
+      <StatusBar translucent={true} barStyle={'light-content'} backgroundColor={'transparent'} />
+      <ImageBackground style={styles.bg} source={require('../assets/imgs/bg.png')}>
+        <Header title="主页" fullScreen />
+      </ImageBackground>
+      <View style={styles.buttonWrapper}>
+        <Button
+          title="退出登录"
+          onPress={() => props.navigation.push('Login')}
+          color="#437dff"
+        />
+      </View>
+    </View>
+  )
+}
+// export default class Home extends React.PureComponent {
+//   static navigationOptions = {
+//     title: '主页'
+//   }
+
+//   render() {
+//   }
+// }
+
+const styles = StyleSheet.create({
+  fill: {
+    flex: 1
+  },
+  bg: {
+    height: 234,
+  },
+  text: {
+    fontSize: 20,
+    fontWeight: '500',
+    color: '#437dff',
+    textAlign: 'center'
+  },
+  buttonWrapper: {
+    padding: 16
+  }
+})
